9f25dcb22935454f10e10dbc2da4be76ba24aef3
JeanJano <jeansauvage6@gmail.com>
2025-06-25 18:23:47 +0200

    init project

---
+++ describe.py
@@ -0,0 +1,13 @@
+def main():
+
+    line = [][]
+    with open("./datasets/dataset_train.csv", 'r') as f:
+        data = f.read()
+        i = 0
+        # for i in data:
+        #     print(i)
+        #     if i == "\n":
+        #         break
+
+if __name__ == "__main__":
+    main()
