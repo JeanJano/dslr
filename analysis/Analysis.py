0c9aa281cb27c636584f1addcf13d3c6badedc23
JeanJano <jeansauvage6@gmail.com>
2025-06-26 18:56:35 +0200

    reading csv and start class

---
+++ analysis/Analysis.py
@@ -0,0 +1,4 @@
+class Analysis:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        print("constructor", self.file_path)
--- describe.py
+++ describe.py
@@ -1,9 +1,25 @@
+from analysis.Analysis import Analysis
+
 def main():
 
-    line = [][]
+    analysis = Analysis("./datasets/dataset_train.csv")
+
+    lines = []
     with open("./datasets/dataset_train.csv", 'r') as f:
-        data = f.read()
+        lines = f.readlines()
         i = 0
+        for line in lines:
+            if i == 0:
+                i += 1
+                continue
+
+            line_split = line.split(",")
+            print(line_split)
+            i += 1
+            if i > 1:
+                break
+
+    # print(lines[0])
         # for i in data:
         #     print(i)
         #     if i == "\n":
